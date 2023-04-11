edde5abcb1e3dda7c357bb32f6ad1c11a0eb26ab
abdulmananoxibit <96009509+abdulmananoxibit@users.noreply.github.com>
2023-04-11 14:27:51 +0500

    functionality added

--- app/src/main/java/com/mikronexus/cfa/MainActivity.kt
+++ app/src/main/java/com/mikronexus/cfa/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.mikronexus.cfa
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.ComponentName
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var button: MaterialButton
+    lateinit var textView: TextView
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button = findViewById(R.id.proceed)
+        textView = findViewById(R.id.packageName)
+        textView.text = "" + packageName
+        button.setOnClickListener {
+            val intent = Intent(Intent.ACTION_MAIN)
+            intent.component =
+                ComponentName("com.mikronexus.dejavoo", "com.mikronexus.dejavoo.MainActivity")
+            try {
+                startActivity(intent)
+            } catch (e: Exception) {
+                Toast.makeText(
+                    this, "Please Install the other Application to Proceed", Toast.LENGTH_SHORT
+                ).show()
+                e.printStackTrace()
+            }
+
+
+        }
+
     }
 }
